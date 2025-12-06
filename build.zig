f17669c362350e18c78a0831e74421543d30e863
Joris Pevcevičius <joris.pevcas@gmail.com>
2025-12-06 16:54:31 +0100

    build: support for zig build check

--- build.zig
+++ build.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
+fn buildC(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Module {
     const c_translate = b.addTranslateC(.{
         .link_libc = false,
         .target = target,
@@ -13,7 +10,6 @@ pub fn build(b: *std.Build) void {
             \\#include <libelf.h>
         ),
     });
-
     const nix_cflags: []const u8 = std.process.getEnvVarOwned(b.allocator, "NIX_CFLAGS_COMPILE") catch "";
     const nix_cflags_trimmed: []const u8 = std.mem.trim(u8, nix_cflags, " \n\t");
     var it = std.mem.tokenizeScalar(u8, nix_cflags_trimmed, ' ');
@@ -28,24 +24,40 @@ pub fn build(b: *std.Build) void {
             }
         }
     }
+    return c_translate.createModule();
+}
 
-    const c = c_translate.createModule();
-
+fn buildRemuStep(b: *std.Build, root_module: *std.Build.Module, use_llvm: bool) *std.Build.Step.Compile {
     const exe = b.addExecutable(.{
         .name = "bemu",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{.name = "c", .module = c },
-            },
-        }),
+        .root_module = root_module,
         .link_libc = true,
+        .use_llvm = use_llvm,
     });
-
     exe.linkSystemLibrary("elf");
     exe.linkSystemLibrary("c");
+    return exe;
+}
 
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const use_llvm = b.option(bool, "use_llvm", "Force use llvm or not") orelse true;
+
+    const c = buildC(b, target, optimize);
+    const root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{.name = "c", .module = c },
+        },
+    });
+
+    const exe = buildRemuStep(b, root_module, use_llvm);
     b.installArtifact(exe);
+
+    const check_step = b.step("check", "Check if remu compiles");
+    const exe_check = buildRemuStep(b, root_module, false);
+    check_step.dependOn(&exe_check.step);
 }
