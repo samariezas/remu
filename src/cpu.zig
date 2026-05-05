20dbb087b85099b50fb85a5562f46271719dc731
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-05-05 20:07:51 +0300

    FIX INSTRUCTION CACHE

--- src/cpu.zig
+++ src/cpu.zig
@@ -672,7 +672,7 @@ pub fn RVCPU(comptime opt: cpu_config.CpuOptions) type {
                 Instr.makeDirect("MRET", 0x30200073, handleMret, null).noJump(),
                 Instr.makeDirect("SRET", 0x10200073, handleSret, null).noJump(),
 
-                Instr.makeF37("SFENCE.VMA", 0b1110011, 0, 9, handleNop, null),
+                Instr.makeF37("SFENCE.VMA", 0b1110011, 0, 9, handleFenceI, null),
             } else [_]Instr {}) ++
             (if (opt.a_extension) [_]Instr {
                 Instr.makeF35("AMOSWAP.W", 0b0101111, 0b010, 0b00001,  handleAmoswapW,  null),
@@ -981,6 +981,7 @@ pub fn RVCPU(comptime opt: cpu_config.CpuOptions) type {
             mode: u4,
 
             fn handleWrite(cpu: *Self, value: Tword) void {
+                cpu.instruction_cache.updateGeneration();
                 const parsed: SatpCSR = @bitCast(value);
                 cpu.ppn = parsed.ppn;
                 cpu.asid = parsed.asid;
