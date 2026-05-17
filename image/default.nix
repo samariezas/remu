6970fc87ca3ddddd7d054140ac08b4e7a4e7fb9c
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-05-17 23:54:22 +0300

    feat: add hello world sample

--- image/default.nix
+++ image/default.nix
@@ -6,10 +6,20 @@ rec {
   utils = import ./utils.nix { inherit pkgsCross pkgs; };
   tcc = import ./tcc.nix { inherit pkgsCross pkgs; };
 
+  samples = pkgs.stdenv.mkDerivation {
+    name = "samples";
+    src = ./samples;
+    buildPhase = ''
+      mkdir -p $out/samples
+      cp -a * $out/samples
+    '';
+    dontFixup = true;
+  };
+
   linux = import ./linux.nix { inherit pkgsCross pkgs; };
   initramfs = import ./initramfs.nix {
     inherit pkgs;
-    derivations = [ musl busybox lua utils tcc ];
+    derivations = [ musl busybox lua utils tcc samples ];
   };
   opensbi = import ./opensbi.nix { inherit pkgsCross pkgs; };
 }
---
+++ image/samples/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -xe
+tcc hello.c -o hello
