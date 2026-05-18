f084e8cdc6bb14938a9746d2b66b0c8e73f70436
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-05-18 17:04:27 +0300

    feat: allow running via 'nix run'

--- flake.nix
+++ flake.nix
@@ -29,7 +29,29 @@
         tests.spike-mod
         dtc
       ];
-    in {
+
+      run-script = pkgs.writeScript "run.sh" ''
+        #!/usr/bin/env bash
+        set -eu
+        cd "$(dirname "$0")"
+        
+        if [ -x "../remu.AppImage" ]; then
+            EMULATOR="../remu.AppImage"
+        elif [ -x "./remu.AppImage" ]; then
+            EMULATOR="./remu.AppImage"
+        else
+            echo "Error: remu.AppImage not found or does not have execute permission." >&2
+            exit 1
+        fi
+        
+        set -x
+        "$EMULATOR" binary \
+            ./opensbi.bin \
+            ./machine.dtb \
+            ./linux \
+            ./initrd
+      '';
+    in rec {
       packages.${system} = with tests; {
         inherit riscv-tests
                 riscv-tests-orig;
@@ -76,6 +98,8 @@
             cp ${image.initramfs} ./remu-package/initrd
             cp ${image.linux}/Image ./remu-package/linux
             cp ${image.opensbi}/fw_dynamic.bin ./remu-package/opensbi.bin
+            cp ${run-script} ./remu-package/run.sh
+            chmod +x ./remu-package/run.sh
             INITRD_SIZE=$(printf "%07x" "$(stat -c %s ./remu-package/initrd)")
             sed "s/{INITRD_SIZE}/$INITRD_SIZE/" ${./simple.dts.template} | ${pkgs.dtc}/bin/dtc > ./remu-package/machine.dtb
             tar cvzf $out ./remu-package
@@ -91,12 +115,27 @@
         riscv-musl = pkgsCross.mkShell { };
       };
 
-      apps.${system}.tests = {
-        type = "app";
-        program = "${pkgs.writeShellScript "run_riscv_tests" ''
-          zig build
-          ./zig-out/bin/remu full ${tests.riscv-tests}/share/riscv-tests/
-        ''}";
+      apps.${system} = {
+        tests = {
+          type = "app";
+          program = "${pkgs.writeShellScript "run_riscv_tests" ''
+            zig build
+            ./zig-out/bin/remu full ${tests.riscv-tests}/share/riscv-tests/
+          ''}";
+        };
+      
+        default = {
+          type = "app";
+          program = "${pkgs.writeShellScript "run_riscv_tests" ''
+            INITRD_SIZE=$(printf "%07x" "$(stat -c %s ${image.initramfs})")
+            ${packages.${system}.remu}/bin/remu binary \
+                ${image.opensbi}/fw_dynamic.bin \
+                <(sed "s/{INITRD_SIZE}/$INITRD_SIZE/" ${./simple.dts.template} | ${pkgs.dtc}/bin/dtc) \
+                ${image.linux}/Image \
+                ${image.initramfs}
+          ''}";
+
+        };
       };
     };
 }
